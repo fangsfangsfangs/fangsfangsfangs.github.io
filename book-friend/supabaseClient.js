4f50442a16142f3cea03c81f04224a6398311639
fangsfangsfangs <fangsfangsfangs@proton.me>
2025-07-09 23:53:34 -0700

    Delete book-friend/supabaseClient.js

--- book-friend/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://pnpjlsjxlbrihxlkirwj.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBucGpsc2p4bGJyaWh4bGtpcndqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIwODIyNDMsImV4cCI6MjA2NzY1ODI0M30.KP6YZtDGsH6_MtSJF03r2nhmEcXTvpd4Ppb-M3HYkhg";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
